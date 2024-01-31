f037909b479751ff1bf21201ecdb52a72cb87d19
Ganesh-1907 <bora1132004@gmail.com>
2024-01-31 11:54:39 +0530

    navbar and home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import { BrowserRouter , Route , Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/home';
+import Navbar from './components/navbar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      
-   
-    </div>
+    <>
+    
+    <ThemeProvider theme={darkTheme}>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      {/* <Route path='/navbar' element={<Navbar/>}></Route> */}
+
+    </Routes>
+    </BrowserRouter>
+        </ThemeProvider>
+    </>
   );
 }
 
---
+++ client/src/components/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./navbar";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+
+const Home = () => {
+  return (
+    <>
+    <Navbar />
+   
+    </>
+  );
+};
+
+export default Home;
